1c1118d513933881e683d525e5af130a9eac9c75
Jorrit Salverda <jorrit.salverda@lightyear.one>
2022-07-08 09:15:51 +0200

    continue underfloor heating post

--- app/root.tsx
+++ app/root.tsx
@@ -32,11 +32,11 @@ export default function App() {
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
           <div className="relative h-44 flex items-center justify-end sm:justify-center">
             <Link to="/" className="absolute left-0">
-              <img src="/go_green.png" width="100" />
+              <img src="/go_green.png" width="100" alt="logo" />
             </Link>
             <Link to="/" className="text-center w-9/12 flex-initial sm:w-full sm:flex-auto leading-none font-medium">
               <h1 className="text-5xl text-lime-600">Going Green</h1>
-              <h6 className="text-md italic text-slate-500">Step by step</h6>
+              <h6 className="text-md italic text-slate-500">For a brighter future</h6>
             </Link>
           </div>
           <div className="pt-6">
