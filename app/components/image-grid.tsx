838526f75ec743655c43eee86e57ae5fb5aea100
Jorrit Salverda <jorrit.salverda@lightyear.one>
2022-07-16 08:18:45 +0200

    grid shows larger image for 1 image

--- app/components/image-grid.tsx
+++ app/components/image-grid.tsx
@@ -7,6 +7,19 @@ export default function ImageGrid({
   basePath: String,
   images: Array<string>,
 }) {
+  if (images.length == 1) {
+    return (
+      <>
+        {images.map((image, index) => (
+          <Link to={`./${image}`} key={index}>
+            <img src={basePath + image} alt="" />
+          </Link>
+        ))}
+      </>
+    );
+  }
+
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-3 gap-3 bg-white">
       {images.map((image, index) => (
