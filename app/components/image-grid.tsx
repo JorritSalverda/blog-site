f51250197566f605379114d05b6be4d50b9d6a98
Jorrit Salverda <jorrit.salverda@lightyear.one>
2022-07-16 06:26:46 +0200

    create image route for showing image grid image in modal

--- app/components/image-grid.tsx
+++ app/components/image-grid.tsx
@@ -1,12 +1,18 @@
+import { Link } from "@remix-run/react";
+
 export default function ImageGrid({
+  basePath,
   images,
 }: {
+  basePath: String,
   images: Array<string>,
 }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-3 gap-3 bg-white">
       {images.map((image, index) => (
-        <img src={image} className="w-full aspect-square object-cover" alt="" key={index} />
+        <Link to={`./${image}`} key={index}>
+          <img src={basePath + image} className="w-full aspect-square object-cover" alt="" />
+        </Link>
       ))}
     </div>
   );
---
+++ app/routes/posts.$post.$image.tsx
@@ -0,0 +1,26 @@
+import type { LoaderFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare"
+import { useLoaderData } from "@remix-run/react";
+import { Link } from "@remix-run/react";
+
+type LoaderData = { post: string, image: string };
+
+export const loader: LoaderFunction = async ({
+  params,
+}) => {
+  return json({ post: params.post, image: params.image });
+};
+
+export default function ImageRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <Link to={`/posts/${data.post}`}>
+      <div className="bg-slate-900/50 fixed top-0 left-0 right-0 bottom-0 flex items-center">
+        <div className="w-full h-full md:w-11/12 md:h-5/6 m-auto flex-none">
+          <img src={`/media/${data.post}/${data.image}`} alt="" className="max-w-full max-h-full" />
+        </div>
+      </div>
+    </Link>
+  );
+}
