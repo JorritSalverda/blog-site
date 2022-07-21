e83905eed5ed8953e831707b496aaa8511396fbb
Jorrit Salverda <jorrit.salverda@lightyear.one>
2022-07-21 15:08:54 +0200

    add green roof post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,9 @@
 import * as postUnderfloorHeating from "../routes/posts/underfloor-heating.mdx";
 import * as postSolarPanels from "../routes/posts/solar-panels.mdx";
 import * as postGroundSourceHeatPump from "../routes/posts/ground-source-heat-pump.mdx";
+import * as postGreenRoof from "../routes/posts/green-roof.mdx";
+import * as postElectricMobility from "../routes/posts/electric-mobility.mdx";
+import * as postLoadShifting from "../routes/posts/load-shifting.mdx";
 
 export type Post = {
   slug: string;
@@ -21,10 +24,14 @@ function postFromModule(module: any): Post {
 }
 
 export async function getPosts(): Promise<Array<Post>> {
+
   const posts = [
     postFromModule(postUnderfloorHeating),
     postFromModule(postSolarPanels),
     postFromModule(postGroundSourceHeatPump),
+    postFromModule(postGreenRoof),
+    // postFromModule(postElectricMobility),
+    // postFromModule(postLoadShifting),
   ];
 
   const sortedPosts = posts.sort(
