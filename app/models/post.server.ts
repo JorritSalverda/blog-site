ddde05c64d8fa48d7fe00c900d39db0ed770210d
Jorrit Salverda <jorrit.salverda@lightyear.one>
2022-08-24 14:38:11 +0200

    the future is electric post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -4,6 +4,7 @@ import * as postGroundSourceHeatPump from "../routes/posts/ground-source-heat-pu
 import * as postGreenRoof from "../routes/posts/green-roof.mdx";
 import * as postElectricMobility from "../routes/posts/electric-mobility.mdx";
 import * as postLoadShifting from "../routes/posts/load-shifting.mdx";
+import * as postTheFutureIsElectric from "../routes/posts/the-future-is-electric.mdx";
 
 export type Post = {
   slug: string;
@@ -32,6 +33,7 @@ export async function getPosts(): Promise<Array<Post>> {
     postFromModule(postGreenRoof),
     postFromModule(postElectricMobility),
     postFromModule(postLoadShifting),
+    postFromModule(postTheFutureIsElectric),
   ];
 
   const sortedPosts = posts.sort(
